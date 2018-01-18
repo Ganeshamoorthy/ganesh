c10744753d35c984540e1cebce93a49af3f293e2
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-01-18 15:56:20 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int number;
-
+    int num;
     printf("Enter an integer: ");
     scanf("%d", &number);
     if(number % 2 == 0)
-        printf("%d is even.", number);
+        printf("%d is even.", num);
     else
-        printf("%d is odd.", number);
-
+        printf("%d is odd.", num);
     return 0;
 }
